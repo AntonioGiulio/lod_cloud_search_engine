5a50a15307f6c273de7da2b5c0d0b1fe5b04f10a
AntonioGiulio <antonio.giulio96@gmail.com>
2021-06-24 20:04:45 +0200

    Aggiunta metodi brutalSearch, tagSearch, multiTagSearch

--- npm_module/index.js
+++ npm_module/index.js
@@ -1,6 +1,9 @@
 //Adesso cerchiamo di rendere un modulo a parte tutto quello 
 // che abbiamo realizzato per interrogare lod-cloud
 const http_tool = require ('xmlhttprequest').XMLHttpRequest;
+const graphBuilder = require('ngraph.graph');
+const pagerank = require('ngraph.pagerank');
+const centrality = require('ngraph.centrality');
 
 class LC_Querier {
     
@@ -13,10 +16,55 @@ class LC_Querier {
         request.send();
         if(request.status === 200){
             console.log('Status code response from lod-cloud: ', request.status);
-            const datasets = JSON.parse(request.responseText);
-            console.log(datasets);
+            this.datasets = JSON.parse(request.responseText);
         }
     } 
+
+    brutalSearch(target){
+        var results = JSON.parse('[]');
+        var i = 0;
+        var field;
+        const pattern = new RegExp(target, 'i');
+        console.log(pattern);
+        for(let d in this.datasets){
+            field = JSON.stringify(this.datasets[d]);
+            if(pattern.test(field))
+                results[i++] = this.datasets[d];            
+        }
+        return results;
+    }
+
+    tagSearch(target, tag){
+        var results = JSON.parse('[]');
+        var i = 0;
+        var field;
+        const pattern = new RegExp(target, 'i');
+        for(let d in this.datasets){
+            field = JSON.stringify(this.datasets[d][tag]);
+            if(pattern.test(field))
+                results[i++] = this.datasets[d];
+        }
+
+        return results;    
+    }
+
+    multiTagSearch(target, ...tags){
+        var results = JSON.parse('[]');
+        var i = 0, j;
+        var field;
+        const pattern = new RegExp(target, 'i');
+        for(let d in this.datasets){
+            field = '';
+            for(j in tags){
+                field += JSON.stringify(this.datasets[d][tags[j]]);
+            }
+            if(pattern.test(field)){
+                results[i++] = this.datasets[d];
+            }
+        }
+
+        return results;
+    }
 }
 
 module.exports = LC_Querier;
