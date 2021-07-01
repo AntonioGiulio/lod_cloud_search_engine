68d0f53d12105a1cec393de8d3865af7a06d7e08
AntonioGiulio <antonio.giulio96@gmail.com>
2021-07-01 20:08:08 +0200

    Aggiunta di documentazione per il modulo npm lodcloud-querier

---
+++ npm_module/README.md
@@ -0,0 +1,60 @@
+# Lod-Cloud Querier
+
+## Description 
+It is a javascript class created to make it easier and more effective to search for knowledge graphs among those made available by [lod-cloud](https://lod-cloud.net).
+
+The site provides a [json file](https://lod-cloud.net/lod-data.json) that we analyze and query, in particular you can make a **brutal search** and a **search by tag**.
+
+You can also choose to **rank** the results in 4 different ways:
+
+* **name** (default)
+* **size**
+* **authority**
+* **centrality**
+
+
+## Basic Usage
+**Install** with npm:
+`npm install lodcloud-querier`
+
+```javascript
+// First of all you have to require the package in the code
+var lc_querier = require('./index');
+
+// ..then you have to initialize the lc_querier
+var querier = new lc_querier();
+```
+
+Now you are ready to exploit all the functions:
+
+```javascript
+BRUTAL SEARCH
+
+var results = querier.brutalSearch('keyword', 'rankingMode'); 
+//rankingMode(optional) is one of['name', 'size', 'authority', 'centrality']
+
+
+TAG SEARCH
+
+var results = querier.tagSearch('keyword', 'tag', rankingMode); 
+//choose one of the tags from lodcloud json structure.
+
+
+MULTITAG SEARCH
+
+var results = querier.multiTagSearch('keyword', 'tag_1', 'tag_2', 'tag_3', ...,  rankingMode);
+// you perform the query on several tags.
+
+```
+
+## Available methods for a  lodcloud-querier instance
+
+* **brutalSearch(target)** : For each knowledge graph in lod-cloud, it searches within all tags for the regular expression containing the target.
+* **tagSearch(target, tag)**: For each knowledge graph, it searches within the specified tag for the regular expression containing the target.
+* **multiTagSearch(target, ...tags)**: For each knowledge graph, it searches within the specified tags the regular expression containing the target.
+* **filterResults(result, ...tags)**: It's a filter to return in the resulting JSON only tags specified.
+* **generalSorting(result, mode)**: It's a dispatcher method to execute the ranking algorithm specified in mode parameter.
+* **sortResultsBySize(results)**: Sorts results by triples number.
+* **sortResultsByName(results)**: Sorts results in alphabetic order using the identifier.
+* **sortResultsByAuthority(results)**: Sorts results by authority using the pagerank algorithm.
+* **sortResultsByCentrality(results)**: Sorts results by centrality using the centrality algorithm.
--- npm_module/index.js
+++ npm_module/index.js
@@ -1,25 +1,45 @@
-//Adesso cerchiamo di rendere un modulo a parte tutto quello 
-// che abbiamo realizzato per interrogare lod-cloud
+/* 
+– – – – – – – – – – – – – – – – – – – – – – – – – – – – – 
+Title : Lod Cloud Querier
+Author : Antonio Giulio
+URL : https://github.com/AntonioGiulio/lod_cloud_search_engine
+Description : This module is capable of performing various types of queries on the JSON file offered by lod cloud.
+            It turns out to be useful for those looking for a Knowledge Graph to use 
+            or for those who want to create analytics on lodcloud.
+Created : July 1 2021
+version : 0.1.0
+– – – – – – – – – – – – – – – – – – – – – – – – – – – – – 
+*/
+
+// npm module required for http request/response
 const http_tool = require ('xmlhttprequest').XMLHttpRequest;
+// npm module required to create graphs
 const graphBuilder = require('ngraph.graph');
+// npm module required to compute pagerank on a graph
 const pagerank = require('ngraph.pagerank');
+// npm module required to compute centrality on a graph
 const centrality = require('ngraph.centrality');
 
 class LC_Querier {
     
-
     constructor() {
-        //Per prima cosa dobbiamo effettuare una richiesta GET sincrona verso l'end di LC
-        // in modo da ottenere l'oggetto JSON su cui fare le interrogazioni
+        
         var request = new http_tool();
-        request.open('GET', "https://lod-cloud.net/lod-data.json", false);
+        request.open('GET', "https://lod-cloud.net/lod-data.json", false);      //GET request to lod-cloud endpoint
         request.send();
         if(request.status === 200){
             console.log('Status code response from lod-cloud: ', request.status);
-            this.datasets = JSON.parse(request.responseText);
+            this.datasets = JSON.parse(request.responseText);       //parsing in JSON of response file
         }
     } 
 
+
+    /*
+    * Summary: For each knowledge graph, it searches within all tags 
+                for the regular expression containing the target.
+    * Parameters: target (string) keyword to search, rankingMode (string) enable one of ranking modes.
+    * Return: JSONArray containing the ordered results.
+    */
     brutalSearch(target){
         var results = JSON.parse('[]');
         var i = 0;
@@ -35,6 +55,12 @@ class LC_Querier {
         return this.generalSorting(results, arguments[1]);
     }
 
+    /*
+    * Summary: For each knowledge graph, it searches within the specified tag 
+                for the regular expression containing the target.
+    * Parameters: target (string) keyword to search, tag (string) tag to inspect, rankingMode (string) enable one of ranking modes.
+    * Return: JSONArray containing the ordered results.
+    */
     tagSearch(target, tag){
         var results = JSON.parse('[]');
         var i = 0;
@@ -48,7 +74,14 @@ class LC_Querier {
 
         return this.generalSorting(results, arguments[2]);
     }
+    
 
+    /*
+    * Summary: For each knowledge graph, it searches within the specified tags 
+                for the regular expression containing the target.
+    * Parameters: target (string) keyword to search, tags (string[]) tags to inspect, rankingMode (string) enable one of ranking modes.
+    * Return: JSONArray containing the ordered results.
+    */
     multiTagSearch(target, ...tags){
         var results = JSON.parse('[]');
         var i = 0, j;
@@ -69,6 +102,11 @@ class LC_Querier {
     }
 
 
+    /*
+    * Summary: It's a filter to return in the resulting JSON only tags specified.
+    * Parameters: results (JSONArray) generated in a previous request, tags (string[]) tag to filter in.
+    * Return: JSONArray containing the filtered results.
+    */
     filterResults(result, ...tags){
         var filteredResults = JSON.parse('[]');
         var j, z = 0;
@@ -83,6 +121,11 @@ class LC_Querier {
         return filteredResults
     }
 
+    /*
+    * Summary: It's a dispatcher method to execute the ranking algorithm specified in mode parameter.
+    * Parameters: results (JSONArray) generated in a previous request, mode (string) ranking mode.
+    * Return: JSONArray containing the ordered results.
+    */
     generalSorting(result, mode){
         switch(mode){
             case 'size':
@@ -106,6 +149,11 @@ class LC_Querier {
         }
     }
 
+    /*
+    * Summary: Sorts results by triples number.
+    * Parameters: results (JSONArray) generated in a previous request.
+    * Return: JSONArray containing the ordered results.
+    */
     sortResultsBySize(results){
         console.log('ordinamento per size');
         results.sort(function(a, b){ return b.triples - a.triples});
@@ -113,6 +161,12 @@ class LC_Querier {
         return results;
     }
 
+
+    /*
+    * Summary: Sorts results in alphabetic order using the identifier.
+    * Parameters: results (JSONArray) generated in a previous request.
+    * Return: JSONArray containing the ordered results.
+    */
     sortResultsByName(results){
         console.log('Ordinamento in ordine alfabetico');
         results.sort(function(a, b){
@@ -126,9 +180,15 @@ class LC_Querier {
         return results;
     }
 
+
+    /*
+    * Summary: Sorts results by authority using the pagerank algorithm
+    * Parameters: results (JSONArray) generated in a previous request.
+    * Return: JSONArray containing the ordered results.
+    */
     sortResultsByAuthority(results){
         console.log('Ordinamento per authority in base al pagerank');
-        var resultGraph = createGraph(results);
+        var resultGraph = createGraph(results);   
         var rank = pagerank(resultGraph);
         console.log(rank);
 
@@ -137,6 +197,11 @@ class LC_Querier {
         return results;
     }
 
+    /*
+    * Summary: Sorts results by centrality using the centrality algorithm
+    * Parameters: results (JSONArray) generated in a previous request.
+    * Return: JSONArray containing the ordered results.
+    */
     sortResultsByCentrality(results){
         console.log('Ordinamento per centrality');
         var resultGraph = createGraph(results);
@@ -149,6 +214,11 @@ class LC_Querier {
     }
 }
 
+/*
+* Summary: It's an external function that create a graph from results.
+* Parameters: raw (JSONArray) generated in a previous request.
+* Return: graph.
+*/
 function createGraph(raw){
     //creiamo un grafo vuoto
     var graph = graphBuilder();
